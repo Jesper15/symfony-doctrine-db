2ad86f9cc6ae620c1e47d75bb5419e4d5aba9ef1
jesper15 <230717@edu.rocfriesepoort.nl>
2022-09-23 11:32:09 +0200

    Adding request entity and up/down thumbs

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -6,6 +6,8 @@ use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,12 +35,23 @@ class MixController extends AbstractController
     }
 
     #[route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository)
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
-
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
         ]);
+    }
+
+    #[route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request): Response
+    {
+
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+        dd($mix);
     }
 }
